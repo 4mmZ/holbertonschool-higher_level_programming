75cbfdef7ecbdcd7544d8ba4466a31aa7358d15d
4mmZ <andresmaxi111014@gmail.com>
2023-01-19 09:56:12 -0800

    fix pycodestyle of task 6

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for i in range (0, 10):
-    for n in range (0, 10):
+for i in range(0, 10):
+    for n in range(0, 10):
         if n == i or n < i:
             continue
         print("{}{}".format(i, n), end='')
@@ -8,4 +8,3 @@ for i in range (0, 10):
             print("\n", end='')
             break
         print(', ', end='')
-
