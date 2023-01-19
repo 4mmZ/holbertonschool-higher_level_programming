4aa597436ca75afdb19d7d2ee1a8d825b863c386
4mmZ <andresmaxi111014@gmail.com>
2023-01-19 09:52:54 -0800

    task 6 print comb

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -3,5 +3,9 @@ for i in range (0, 10):
     for n in range (0, 10):
         if n == i or n < i:
             continue
-        print("{}{}".format(i, n))
+        print("{}{}".format(i, n), end='')
+        if i == 8 and n == 9:
+            print("\n", end='')
+            break
+        print(', ', end='')
 
