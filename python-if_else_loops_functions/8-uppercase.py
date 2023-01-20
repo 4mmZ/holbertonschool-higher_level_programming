2fd026c386158db4edc4bd75218a65a36442a244
4mmZ <andresmaxi111014@gmail.com>
2023-01-20 03:47:36 -0800

    task 8 "string to uppercase"

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def uppercase(str):
+    ouput = ''
+    for ch in str:
+        if ch >= 'a' and ch <= 'z':
+            var1 = ord(ch)
+            var2 = var1 - 32
+            ouput = ouput+chr(var2)
+        else:
+            ouput = ouput+ch
+    print("{}".format(ouput))
