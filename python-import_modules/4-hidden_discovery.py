993f358cf53c2144ed213fdcd76702060c1611fd
4mmZ <andresmaxi111014@gmail.com>
2023-01-20 09:21:06 -0800

    task 4, prints all the names in a compiled module

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -2,7 +2,8 @@
 import hidden_4
 
 
-name = dir(hidden_4)
-for n in name:
-    if not n.startswith('__'):
-        print("{}".format(n))
+if __name__ == '__main__':
+    name = dir(hidden_4)
+    for n in name:
+        if not n.startswith('__'):
+            print("{}".format(n))
