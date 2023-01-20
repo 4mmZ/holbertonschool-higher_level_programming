b6126da1099ad3c7d5e1ce43c9b5e581e08c6bac
4mmZ <andresmaxi111014@gmail.com>
2023-01-20 04:21:23 -0800

    task 9, "print last digit"

---
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+def print_last_digit(number):
+    if number < 0:
+        digit = number
+        digit = digit * -1
+        digit = digit % 10
+    else:
+        digit = number
+        digit = digit % 10
+    print("{}".format(digit), end='')
+    return digit
