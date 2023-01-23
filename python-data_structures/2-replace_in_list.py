618265f7b11d5f5031cf91baaa21334e06ffbabc
Andres <andresmaxi111014@gmail.com>
2023-01-23 07:42:03 -0800

    task 2, fix "if conditions"

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -3,9 +3,9 @@
 def replace_in_list(my_list, idx, element):
     if idx < 0:
         return my_list
-    if idx > len(my_list):
-        return my_list
-    for n in range(len(my_list)):
-        if n == idx:
+    for n in range(0, len(my_list)):
+        if idx > n:
+            return my_list
+        elif n == idx:
             my_list[n] = element
             return my_list
