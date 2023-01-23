26027c6d300425e5030fc5e1dcacf906c776ffb5
Andres <andresmaxi111014@gmail.com>
2023-01-23 07:34:06 -0800

    task 2 "fix a fix condition"

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 def replace_in_list(my_list, idx, element):
+    if ixd < 0:
+        return my_list
     if idx > len(my_list):
         return my_list
     for n in range(len(my_list)):
