537dd4e85ef49f06bda24e4c2d37f86c7ee2b03f
4mmZ <andresmaxi111014@gmail.com>
2023-01-24 07:16:39 -0800

    task 6, fuction that ennumerate a matrix

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -3,8 +3,8 @@
 
 def print_matrix_integer(matrix=[[]]):
     for n in matrix:
-        for i,j in enumerate(n):
+        for i, j in enumerate(n):
             print("{:d}".format(j), end="")
-            if i < len(n) -1:
+            if i < len(n) - 1:
                 print(" ", end='')
         print("")
