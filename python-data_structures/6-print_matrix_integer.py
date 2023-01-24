5bd3ffdc5d7cfc3bb6f02910b9417870c3a4cef5
4mmZ <andresmaxi111014@gmail.com>
2023-01-24 07:15:02 -0800

    task 6

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -3,8 +3,8 @@
 
 def print_matrix_integer(matrix=[[]]):
     for n in matrix:
-        for i in n:
-            if i == n:
-                continue
-            print("{:d}".format(i), end=" ")
-        print ("")
+        for i,j in enumerate(n):
+            print("{:d}".format(j), end="")
+            if i < len(n) -1:
+                print(" ", end='')
+        print("")
