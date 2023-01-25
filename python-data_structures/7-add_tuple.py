fc9f7e1f525b1e5a4e8bd45802ba15fbf120cb2d
4mmZ <andresmaxi111014@gmail.com>
2023-01-25 05:18:07 -0800

    task 7 test

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -8,5 +8,13 @@ def add_tuple(tuple_a=(), tuple_b=()):
         aux = tuple(tuple_aux)
         result1 = add_tuple(tuple_a, aux)
         return result1
+
+    while len(tuple_a) < 2:
+        tuple_aux2 = list(tuple_a)
+        tuple_aux2.append(0)
+        aux2 = tuple(tuple_aux2)
+        result2 = add_tuple(aux2, tuple_b)
+        return result2
+
     result = tuple(map(lambda x, y: x + y, tuple_a, tuple_b))
     return result
