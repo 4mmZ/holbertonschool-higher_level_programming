ee61e4a2eb93a29769e17507c7d2597201aa6ae9
4mmZ <andresmaxi111014@gmail.com>
2023-01-25 05:48:08 -0800

    task 7, add twi tupples

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -2,19 +2,25 @@
 
 
 def add_tuple(tuple_a=(), tuple_b=()):
-    while len(tuple_b) < 2:
-        tuple_aux = list(tuple_b)
-        tuple_aux.append(0)
-        aux = tuple(tuple_aux)
-        result1 = add_tuple(tuple_a, aux)
-        return result1
-
-    while len(tuple_a) < 2:
-        tuple_aux2 = list(tuple_a)
-        tuple_aux2.append(0)
-        aux2 = tuple(tuple_aux2)
-        result2 = add_tuple(aux2, tuple_b)
-        return result2
-
+    if len(tuple_a) < 2 and len(tuple_b) < 2:
+        while len(tuple_b) < 2:
+            tuple_aux = list(tuple_b)
+            tuple_aux.append(0)
+            aux = tuple(tuple_aux)
+            result1 = add_tuple(tuple_a, aux)
+            return result1
+        while len(tuple_a) < 2:
+            tuple_aux2 = list(tuple_a)
+            tuple_aux2.append(0)
+            aux2 = tuple(tuple_aux2)
+            result2 = add_tuple(aux2, tuple_b)
+            return result2
+    if len(tuple_b) > 2 and len(tuple_a) > 2:
+        apl = list(tuple_b)
+        cp = list(tuple_a)
+        apl = tuple(apl[:2])
+        cp = tuple(cp[:2])
+        recu = add_tuple(apl, cp)
+        return recu
     result = tuple(map(lambda x, y: x + y, tuple_a, tuple_b))
     return result
