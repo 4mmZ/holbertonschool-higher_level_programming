d35b13c39b3563b628d5d2b717f8482efe2a80df
4mmZ <andresmaxi111014@gmail.com>
2023-01-25 06:08:56 -0800

    task 9, max integrer

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -4,7 +4,10 @@
 def max_integer(my_list=[]):
     biggest_number = my_list[0]
 
-    for number in my_list:
-        if number > biggest_number:
-            biggest_number = number
-    return biggest_number
+    if my_list:
+        for number in my_list:
+            if number > biggest_number:
+                biggest_number = number
+        return biggest_number
+    else:
+        return None
