3f1a101138cab7d551751dfb0405a3780f81e8d5
4mmZ <andresmaxi111014@gmail.com>
2023-01-25 08:22:00 -0800

    task 11, delete at index

---
+++ python-data_structures/11-delete_at.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+def delete_at(my_list=[], idx=0):
+    if idx < 0 or idx > len(my_list):
+        return my_list
+    for count in range(0, len(my_list)):
+        if count == idx:
+            my_list.remove(count + 1)
+            return my_list
