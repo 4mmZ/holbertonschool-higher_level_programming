c9e6996b3acbe2cad3048308bce5f9755f025d03
4mmZ <andresmaxi111014@gmail.com>
2023-01-30 08:30:01 -0800

    task 2

---
+++ python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+class Scuare with a private instance attribute
+"""
+
+
+class Square:
+    """private instance __size"""
+    __size = None
+
+    def __init__(self, __size) -> None:
+        pass
