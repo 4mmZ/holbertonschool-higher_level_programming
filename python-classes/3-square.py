8cc498c8bb86d89677563d834423f57350733748
4mmZ <andresmaxi111014@gmail.com>
2023-01-31 04:21:44 -0800

    task 3

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -7,7 +7,7 @@ class Square:
     """
     class square
     """
-    def __init__(self, __size):
+    def __init__(self, __size=0):
         self.__size = __size
     def area(self):
         area_aux = int(self.__size) ** 2
