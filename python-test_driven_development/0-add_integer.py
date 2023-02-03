809d17016f2938bb4decf72f9d90e50d74bc5179
4mmZ <andresmaxi111014@gmail.com>
2023-02-03 04:20:20 -0800

    task test 0

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -8,12 +8,15 @@ the message a must be an integer or b must be an integer
 
 
 def add_integer(a, b=98):
+    """
+    def integer
+    """
     if type(a) == float or type(b) == float:
         a = int(a)
         b = int(b)
     if type(a) is not int:
-        raise TypeError("a must be an integer or b must be an integer")
+        raise TypeError("a must be an integer")
     if type(b) is not int:
-        raise TypeError("a must be an integer or b must be an integer")
+        raise TypeError("b must be an integer")
     return a + b
 
