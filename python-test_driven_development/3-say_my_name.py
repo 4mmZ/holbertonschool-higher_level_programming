4ec87aeaa16ddd6b340e975fb5baaf1df7b1adbf
4mmZ <andresmaxi111014@gmail.com>
2023-02-04 09:05:27 -0800

    now the function is documentes

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -4,6 +4,9 @@ Doc
 """
 
 def say_my_name(first_name, last_name=""):
+    """
+    Doc
+    """
     if type(first_name) is not str:
         raise TypeError("first_name must be a string")
     if type(last_name) is not str:
