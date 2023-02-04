31c73a06d556658c3cb8e28ce3b2480e90ad0653
4mmZ <andresmaxi111014@gmail.com>
2023-02-04 08:29:46 -0800

    fix one if statement(case div =! int or float) and test cases

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -11,7 +11,7 @@ def matrix_divided(matrix, div):
     if div == 0:
         raise ZeroDivisionError("division by zero")
     if type(div) is not int and type(div) is not float:
-        raise TypeError("div must be an integer")
+        raise TypeError("div must be a number")
     for count, value in enumerate(matrix):
         if len(matrix) > 0:
             if len(matrix[count - 1]) != len(matrix[count]):
