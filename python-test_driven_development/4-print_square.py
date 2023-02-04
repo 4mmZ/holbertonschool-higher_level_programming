9b87e6890998502fbc351f0b70d9844d8fa436a4
4mmZ <andresmaxi111014@gmail.com>
2023-02-04 09:38:52 -0800

    tast 4 print square and regular tests

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Doc
+"""
+
+
+def print_square(size):
+    square = size
+    if type(square) is not int:
+        raise TypeError("size must be an integer")
+    if square < 0:
+        raise ValueError("size must be >= 0")
+
+    for high in range(square):
+        for lenght in range(square):
+            print("#", end="")
+        print()
