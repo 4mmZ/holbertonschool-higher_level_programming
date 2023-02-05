46eed83bd55634e7b4a0c907d06aef35d49d02f4
4mmZ <andresmaxi111014@gmail.com>
2023-02-05 09:49:01 -0800

    fix ValueError and TypeError statements

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -11,6 +11,15 @@ class Rectangle:
     def __init__(self, width=0, height=0):
         self.__height = height
         self.__width = width
+        if type(self.__height) is not int:
+            raise TypeError("height must be an integer")
+        if self.__height < 0:
+            raise ValueError("height must be >= 0")
+        aux = self.__width
+        if type(aux) is not int:
+                raise TypeError("width must be an integer")
+        if aux < 0:
+                raise ValueError("width must be >= 0")
     @property
     def height(self):
         return self.__height
