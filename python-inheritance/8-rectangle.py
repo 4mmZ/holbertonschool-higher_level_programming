a2f507f8495ec3735aac2a86b98f375c1fc11cbb
4mmZ <andresmaxi111014@gmail.com>
2023-02-09 08:09:21 -0800

    task 8

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Doc """
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+class Rectangle(BaseGeometry):
+    """ Class """
+    def __init__(self, width, height):
+        self.__width = width
+        self.__height = height
+        self.integer_validator(width, 98)
+        self.integer_validator("height", height)
