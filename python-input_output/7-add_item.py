bf2a5c935a4f1f6c44873e3c60cc80c360e19bd6
4mmZ <andresmaxi111014@gmail.com>
2023-02-09 13:42:38 -0800

    task 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
+""" Doc """
+
+
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 from sys import argv as av
 filename = "add_item.json"
-
-
 try:
     my_list = load_from_json_file(filename)
 except FileNotFoundError:
