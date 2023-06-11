d955bc0dc72ef12b735da155e3a20673cabe31ab
4mmZ <andresmaxi111014@gmail.com>
2023-06-11 16:19:32 -0700

    task 0: pycodestyle fix

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -3,7 +3,7 @@ def safe_print_list(my_list=[], x=0):
     try:
         for index in range(x):
             print(("{:d}".format(my_list[index])), end="")
-    except:
+    except Exception:
         print()
         return index
     print()
