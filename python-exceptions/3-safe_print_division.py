8d56aefdd04f7b9feafb3e21e5ae1fde5ca9c4a2
4mmZ <andresmaxi111014@gmail.com>
2023-06-11 16:22:48 -0700

    task 3

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -3,7 +3,7 @@ def safe_print_division(a, b):
     try:
         result = a // b
         print("Inside result: {}".format(float(result)))
-    except:
+    except Exception:
         print("Inside result: {}".format(None))
         return None
     finally:
